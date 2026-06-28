20667db434e91b6148bc6f6771e9738897caa171
macbook_pavel <mail.reborn.web3@gmail.com>
2026-06-28 18:21:41 +0400

    feat: integrate sanity studio at /admin

---
+++ app/admin/[[...index]]/page.tsx
@@ -0,0 +1,8 @@
+import { Studio } from './studio'
+
+// Set the right `viewport`, `robots` and `referer` meta tags
+export { metadata, viewport } from 'next-sanity/studio'
+
+export default function StudioPage() {
+  return <Studio />
+}
---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,5 @@
+import { type SchemaTypeDefinition } from 'sanity'
+
+export const schemaTypes: SchemaTypeDefinition[] = [
+  // Добавьте сюда ваши схемы
+]
